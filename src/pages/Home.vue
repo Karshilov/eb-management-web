041faca33ab89ad6fd63a567c70f0a8a2911a560
Karshilov <karshilov@foxmail.com>
2021-07-20 20:01:28 +0800

    base

---
+++ src/pages/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="basement">
+    <div class="left-side">
+      <div v-for="(item) in options" class="parallel-container item" :key="item.key">
+        <q-icon :name="item.icon" style="color: #fff;" class="icon" />
+        <span style="color: #fff; font-size: 1.2rem; font-weight: bold;">{{item.name}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, toRefs } from 'vue';
+import options from '../utils/options'
+
+export default defineComponent({
+  name: 'PageIndex',
+  props: {
+    title: String
+  },
+  setup(props) {
+    console.log(props)
+  },
+  data() {
+    return {
+      options,
+    }
+  }
+});
+</script>
---
+++ src/utils/options.ts
@@ -0,0 +1,31 @@
+import { matApartment, matGroups, matTimeline, matRssFeed, matVpnKey } from '@quasar/extras/material-icons'
+
+const options = [
+  {
+    key: '/audit-resources',
+    name: '审核房源',
+    icon: matApartment
+  },
+  {
+    key: '/audit-team',
+    name: '审核团队',
+    icon: matGroups
+  },
+  {
+    key: '/dash-board',
+    name: '房源数据',
+    icon: matTimeline
+  },
+  {
+    key: '/blog-management',
+    name: '博客管理',
+    icon: matRssFeed
+  },
+  {
+    key: '/access-management',
+    name: '权限管理',
+    icon: matVpnKey
+  }
+]
+
+export default options;
