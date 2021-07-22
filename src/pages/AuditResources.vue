a2dcc6e9c43ecb943dc91451353757b693d5210c
Karshilov <karshilov@foxmail.com>
2021-07-22 16:50:15 +0800

    appearence

--- src/pages/AuditResources.vue
+++ src/pages/AuditResources.vue
@@ -1,18 +1,26 @@
 <template>
-  <q-page style="background: #fff;">
+  <q-page style="background: #fff">
     <q-scroll-area
       style="position: absolute; top: 0px; bottom: 0px; left: 0px; right: 0px"
     >
       <div class="list-page-center">
-          <q-table
-            v-if="list.length !== 0"
-            :columns="columns"
-            row-key="id"
-            :rows="list"
-            title="待审核房源"
-            class="header-table"
-          >
-          </q-table>
+        <q-table
+          v-if="list.length !== 0"
+          :columns="columns"
+          row-key="id"
+          :rows="list"
+          title="待审核房源"
+          class="header-table"
+        >
+          <template v-slot:body-cell-action="props">
+            <q-tr :props="props">
+              <q-td key="action" :props="props">
+                <q-btn label="通过" color="primary" :disable="loading" style="margin-right: 10px;" />
+                <q-btn label="拒绝" color="primary" outline :disable="loading" />
+              </q-td>
+            </q-tr>
+          </template>
+        </q-table>
       </div>
     </q-scroll-area>
   </q-page>
@@ -23,7 +31,7 @@
 import { defineComponent, ref } from 'vue';
 import moment from 'moment';
 import { api } from 'boot/axios';
-import { RentDetailModel } from '../utils/DataModel';
+import { RentDetailModel, columns } from '../utils/DataModel';
 
 export default defineComponent({
   name: 'audit-resources',
@@ -31,41 +39,11 @@ export default defineComponent({
     return {
       moment,
       list: ref<RentDetailModel[]>([]),
-      columns: [
-        {
-          name: 'title',
-          label: '房源标题',
-          field: 'title',
-          required: true,
-          align: 'center',
-          headerStyle: 'min-width: 500px',
-        },
-        {
-          name: 'create_time',
-          label: '上传时间',
-          field: 'create_time',
-          format: (val: number) => moment(val * 1000).format('YYYY-MM-DD'),
-          required: true,
-          align: 'center',
-        },
-        {
-          name: 'area',
-          label: '面积（㎡）',
-          field: 'area',
-          required: true,
-          align: 'center',
-        },
-        {
-          name: 'floor',
-          label: '楼层信息',
-          field: 'floor',
-          required: true,
-          align: 'center',
-        },
-      ],
+      columns,
       pageSize: 10,
       pageNum: 1,
       total: 0,
+      loading: false,
     };
   },
   async created() {
@@ -81,14 +59,23 @@ export default defineComponent({
       });
       const success = res.data.success as boolean;
       const reason = res.data.reason as string;
-      const result = res.data.result as {
-        total: number;
-        list: RentDetailModel[];
-      };
       if (success) {
-        console.log(result);
-        this.list = result.list;
-        this.total = result.total;
+        const total = res.data.result.total as number;
+        const list = res.data.result.list as any[];
+        for (let i = 0; i < list.length; i++) {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          const info = await api.get('/user', {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            params: { id: list[i].owner },
+          });
+          if (info.data.success) {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            list[i].owner = info.data.result;
+          }
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        this.list = list;
+        this.total = total;
       } else {
         this.$q.notify({
           type: 'negative',
--- src/utils/DataModel.ts
+++ src/utils/DataModel.ts
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 export interface UserInfoModel {
   username: string;
   email: string;
@@ -29,3 +31,71 @@ export interface RentDetailModel {
   rent_type: string;
   equipments: number;
 }
+
+export const columns = [
+  {
+    name: 'title',
+    label: '房源标题',
+    field: 'title',
+    required: true,
+    align: 'center',
+    headerStyle: 'min-width: 300px',
+  },
+  {
+    name: 'city',
+    label: '房源城市',
+    field: 'city',
+    required: true,
+    align: 'center',
+  },
+  {
+    name: 'price',
+    label: '预期价格（元/月）',
+    field: 'price',
+    require: true,
+    align: 'center',
+  },
+  {
+    name: 'rent_type',
+    label: '结算类型',
+    field: 'rent_type',
+    require: true,
+    align: 'center',
+  },
+  {
+    name: 'create_time',
+    label: '上传时间',
+    field: 'create_time',
+    format: (val: number) => moment(val * 1000).format('YYYY-MM-DD'),
+    required: true,
+    align: 'center',
+  },
+  {
+    name: 'area',
+    label: '面积（㎡）',
+    field: 'area',
+    required: true,
+    align: 'center',
+  },
+  {
+    name: 'floor',
+    label: '楼层信息',
+    field: (row: RentDetailModel) => `${row.floor} / ${row.total_floor}`,
+    required: true,
+    align: 'center',
+  },
+  {
+    name: 'owner',
+    label: '房东/经纪人',
+    field: (row: RentDetailModel) => row.owner.username,
+    required: true,
+    align: 'center',
+  },
+  {
+    name: 'action',
+    label: '操作',
+    field: (row: RentDetailModel) => row,
+    require: true,
+    align: 'center'
+  }
+];
